df250d379beafb6b80f1545f3ba92bf0309cfd33
Eduardo Vernizzi <eduardo.vernizzi@hotmail.com>
2024-11-25 21:30:53 -0300

    update

---
+++ fundamentos-poo/Program.cs
@@ -0,0 +1,28 @@
+﻿using fundamentos_poo.Models;
+
+
+//Encapsulamento
+
+ContaCorrente c1 = new ContaCorrente(123, 1000);
+
+c1.ExibirSaldo();
+c1.Sacar(500);
+c1.ExibirSaldo();
+
+
+
+
+
+
+
+
+
+
+
+
+//Abstração de dados, OBJETO
+//Pessoa p1 = new Pessoa();
+
+//p1.Nome = "Carlos Eduardo";
+//p1.Idade = 25;
+//p1.Apresentar();
