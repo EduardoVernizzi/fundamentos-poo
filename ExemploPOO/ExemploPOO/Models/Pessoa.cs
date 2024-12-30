d13f13f19afec4f725ec0311185a6c000f818260
Eduardo Vernizzi <eduardo.vernizzi@hotmail.com>
2024-12-30 10:44:15 -0300

    update

--- ExemploPOO/ExemploPOO/Models/Pessoa.cs
+++ ExemploPOO/ExemploPOO/Models/Pessoa.cs
@@ -6,7 +6,27 @@ using System.Threading.Tasks;
 
 namespace ExemploPOO.Models
 {
-    internal class Pessoa
+
+    public class Pessoa
     {
+        public string Nome { get; set; }
+        public int Idade { get; set; } 
+        public string Email { get; set; }
+
+        public virtual void Apresentar()
+        {
+            Console.WriteLine($"Olá! Meu nome é {Nome} e tenho {Idade} anos.");
+        }
+
+
+
+
+
     }
+
+
+
+
+
+
 }
--- ExemploPOO/ExemploPOO/Program.cs
+++ ExemploPOO/ExemploPOO/Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ExemploPOO.Models;
+using System;
+
+//Polimorfismo
+
+Aluno a1 = new Aluno();
+a1.Nome = "Victor Vernizzi";
+a1.Idade = 15;
+a1.Email = "teste@teste.com";
+a1.Nota = 10;
+a1.Apresentar();
+
+
+
+
+Professor p1 = new Professor();
+p1.Nome = "Eduardo Vernizzi";
+p1.Idade = 25;
+p1.Salario = 5000;
+p1.Apresentar();
+
+
+//Herança
+//Aluno a1 = new Aluno();
+//a1.Nome = "Eduardo";
+//a1.Idade = 25;
+//a1.Nota = 10;
+//a1.Email = "teste@teste.com";
+//a1.Apresentar();
+
+
+
+
+
+
+
+
+//Encapsulamento
+//ContaCorrente c1 = new ContaCorrente(123, 1000);
+
+//c1.ExibirSaldo();
+//c1.Sacar(500);
+//c1.ExibirSaldo();
+
+
+
+//Pessoa p1 = new Pessoa();
+//p1.Nome = "Carlos Eduardo";
+//p1.Idade = 25;
+//p1.Apresentar();
